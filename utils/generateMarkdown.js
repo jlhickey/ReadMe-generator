21d47ad115bb5c7aabd4cd83486eb78b9cd268a9
Joanne Hickey <jlhickey@me.com>
2021-07-25 12:05:24 -0400

    commit to jlhickey/READMe-generator

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,19 +1,39 @@
-// TODO: Create a function that returns a license badge based on which license is passed in
-// If there is no license, return an empty string
-function renderLicenseBadge(license) { }
-
-// TODO: Create a function that returns the license link
-// If there is no license, return an empty string
-function renderLicenseLink(license) { }
-
-// TODO: Create a function that returns the license section of README
-// If there is no license, return an empty string
-function renderLicenseSection(license) { }
-
-// TODO: Create a function to generate markdown for README
-function generateMarkdown(data) {
-    return `# ${data.title}
-`;
+// generateReadme function populating the README.md
+function generateReadme(answers) {
+    return `
+<h1 align="center">${answers.projectTitle} 👋</h1>
+  
+![badge](https://img.shields.io/badge/license-${answers.license})<br />
+## Description
+${answers.description}
+## Table of Contents
+- [Description](#description)
+- [Installation](#installation)
+- [Usage](#usage)
+- [License](#license)
+- [Contributing](#contributing)
+- [Tests](#tests)
+- [Questions](#questions)
+## Installation
+${answers.installation}
+## Usage
+${answers.usage}
+## License
+![badge](https://img.shields.io/badge/license-${answers.license}-brightgreen)
+<br />
+This application is covered by the ${answers.license} license. 
+## Contributing
+${answers.contributing}
+## Tests
+✏️ ${answers.tests}
+## Questions
+ ${answers.questions}<br />
+<br />
+:octocat: Find me on GitHub: [${answers.username}](https://github.com/${answers.username})<br />
+<br />
+✉️ Email me with any questions: ${answers.email}<br /><br />
+_This README was generated with ❤️, 😳, 😩  by [README-generator](https://github.com/jhickey/ReaDMe-generator) _
+    `;
 }
 
-module.exports = generateMarkdown;
+module.exports = generateReadme;
